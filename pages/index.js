da3c44b0eb2ba5664dc03fc54ae14c5f4c627a45
NaynaJahan <65506240+NaynaJahan@users.noreply.github.com>
2021-12-12 00:02:08 +0600

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head'
-import Header from '../components/header'
-import Nav from '../components/nav'
+import Header from '../components/Header'
+import Nav from '../components/Nav'
 import Results from '../components/Results'
 import requests from '../utils/requests';
 
